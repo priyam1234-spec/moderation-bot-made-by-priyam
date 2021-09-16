5f22a196089f9bce134bfa3ccf82f036d9eccdfb
priyam1234-spec <76897482+priyam1234-spec@users.noreply.github.com>
2021-09-16 12:57:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `abc!`;//your prefix for bot
-exports.Token = `ODg3NTczODQ0NzkyMDY2MDU5.YUGHjg.OSPmPec2ywj4vaxf09MW1I-TtAM`;//your token 
+exports.Prefix = `PREFIX HERE`;//your prefix for bot
+exports.Token = `TOKEN HERE`;//your token 
 exports.Color = `BLUE`;//color   of embed
